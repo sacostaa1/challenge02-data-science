0fe4f91300102a5a49de1f76098e415f967fd4fc
SaraCmque <saramanrique2016@gmail.com>
2026-02-01 20:26:51 -0500

    ai model updated

--- ai_module.py
+++ ai_module.py
@@ -39,7 +39,7 @@ def generate_ai_strategy(df_filtered, api_key, dataset_name):
         """
 
         completion = client.chat.completions.create(
-            model="llama3-70b-8192",
+            model="llama-3.3-70b-versatile",
             messages=[{"role": "user", "content": prompt}],
             temperature=0.7,
             max_tokens=1024
